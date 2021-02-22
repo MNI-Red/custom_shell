18d832efa73887e0def488cc62441880adc5bbc9
Alex Konas <alex.konas@menloschool.org>
2021-02-22 15:58:17 -0800

    yeet

--- red_shell.py
+++ red_shell.py
@@ -2,30 +2,62 @@ import os
 import getpass
 import sys
 import shlex
-
+import subprocess as sbp
 # print(getpass.getuser())
 
+def get_input():
+	background = False
+	command_line = shlex.split(input(str(getpass.getuser()) + " " + str(os.getcwd()) + " > "))
+	print(command_line)
+	if '&' in command_line:
+		# command_line = command_line[:-1]
+		background = True
+	return command_line, background
 
-
+def clean_processes(processes):
+	to_remove = []
+	for proc in processes:
+		if proc.poll() != None:
+			to_remove.append(proc)
+	[processes.pop(proc) for proc in to_remove]
+	
 def loop():
 	cwd = os.getcwd()
+	# past_commands = set()
+	processes = {}
 	while True:
-		command_line = shlex.split(input(str(getpass.getuser()) + " " + str(os.getcwd()) + " > "))
-		print(command_line)
-		command = command_line[0]
-		if command == "ex":
-			sys.exit()
-		elif command == "pwd":
-			print(cwd)
-		elif command == "cd":
-			try:
-				os.chdir(command_line[1])
-				cwd = os.getcwd()
-			except FileNotFoundError as e:
-				print(e)
-				continue
-		else:
-			for root, dirs, files in os.walk(cwd):
-				for file in files:
-					print(file)
+		clean_processes(processes)
+		print(processes)
+		try:
+			command_line, background = get_input()
+			# past_commands.add(command_line)
+			command = command_line[0]
+			if command == "exit":
+				for i in processes:
+					i.kill()
+				sys.exit()
+			elif command == "pwd":
+				print(cwd)
+			elif command == "cd":
+				try:
+					os.chdir(command_line[1])
+					cwd = os.getcwd()
+				except FileNotFoundError as e:
+					print(e)
+					continue
+			else:
+				# if not background:
+				p = sbp.Popen(command_line)
+				processes[p] = p.pid
+				if not background:
+					p.communicate()
+				# else:
+				# 	p = sbp.Popen(command_line, shell=True,
+    #          stdin=None, stdout=None, stderr=None, close_fds=True)
+				# 	p.wait()
+				print(p.pid)
+				# p.communicate()
+				# print(out)
+		except KeyboardInterrupt:
+			continue
 loop()

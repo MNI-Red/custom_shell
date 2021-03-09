006af9adf360549fc3b5e9799495c2b53d780139
Alex Konas <alex.konas@menloschool.org>
2021-03-08 16:49:59 -0800

    uhhhh

--- red_shell.py
+++ red_shell.py
@@ -71,23 +71,25 @@ def sigint(signum, frame):
 # 	p = sbp.Popen(command_line)
 # 	return p, pid
 
-def pipe(commands_to_pipe):
+def pipe(commands_to_pipe, first_in, last_out):
 	first, last = commands_to_pipe[0], commands_to_pipe[-1]
-	process = sbp.Popen(first, stdout=sbp.PIPE)
+	process = sbp.Popen(first, stdin = first_in, stdout=sbp.PIPE)
 	pid_list = [process.pid]
 	process_list = [process]
 	out = process.stdout
 
-	for command in commands_to_pipe[1:-2]:
+	for command in commands_to_pipe[1:-1]:
 		previous = process
+
 		process = sbp.Popen(command, stdin=out, stdout=sbp.PIPE)
 		out = process.stdout
+
 		previous.stdout.close()
+
 		pid_list.append(process.pid)
 		process_list.append(process)
 		
-
-	process = sbp.Popen(last, stdin=out)
+	process = sbp.Popen(last, stdin=out, stdout=last_out)
 	pid_list.append(process.pid)
 	# print(pid_list)
 	full_pid = tuple(pid_list)
@@ -95,14 +97,13 @@ def pipe(commands_to_pipe):
 	# pid_to_command[full_pid] = original
 
 	return pid_list, full_pid, process_list
-	
+
 def pipe_wait(process_list):
 	results = [0]*len(process_list)
 	while process_list:
 		last = process_list.pop(-1)
 		results[len(process_list)] = last.wait()
 	return results
-
 
 def parse_subcommands(command_line):
 	stack = []
@@ -115,10 +116,10 @@ def parse_subcommands(command_line):
 			stack.extend([i[:-i.count(')')]]+[')']*i.count(')'))
 		else:
 			stack.append(i)
-	print(stack)
+	# print(stack)
 	while ')' in stack:
 		stack.pop(-1)
-	print(stack)
+	# print(stack)
 	while '$(' in stack:
 		last_dollar = stack[::-1].index('$(')
 		commands.append(stack[-last_dollar:])
@@ -128,101 +129,87 @@ def parse_subcommands(command_line):
 	# commands.reverse()
 	return commands
 
-def execute_command(command, command_line, original, background, piping, in_redirect, out_redirect, pid_to_process, 
-	pid_to_command):
-	if command == "exit":
-		for i in pid_to_process:
-			if type(i) is tuple:
-				for process in pid_to_process[i]:
-					process.kill()
-				continue
-			pid_to_process[i].kill()
-		sys.exit()
-	elif command == "pwd":
-		print(cwd)
-	elif command == "cd":
-		try:
-			os.chdir(command_line[1])
-			cwd = os.getcwd()
-		except FileNotFoundError as e:
-			print(e)
-			# break
-		except IndexError as i:
-			print("Please enter a directory to switch to.")
-			# break
-	elif command == "jobs":
-		if len(pid_to_command) > 0:
-			print()
-			for pid in pid_to_command:
-				# print(proc.args)
-				print("PID: " + str(pid) + ") " + pid_to_command[pid])
-			print()
-	elif command == "fg":
-		if len(command_line) < 2 or not command_line[1].isdecimal():
-			print("Please enter PID of process to foreground")
-			return
-		pid = int(command_line[1])
-		# print(pid)
-		# print(list(pid_to_process.keys()))
-		# print(pid in list(pid_to_process.keys()))
-		if pid not in list(pid_to_process.keys()):
-			print("Please enter the PID of an existing process. Find the PIDs by using the \'jobs\' command.")
-			return
-		arguments = pid_to_process[pid].args
-		pid_to_process[pid].send_signal(signal.SIGCONT)
-		p = sbp.Popen(arguments[:-1])
-		pid_to_process[p.pid] = p
-		pid_to_command[p.pid] = original
-	elif command == "bg":
-		if len(command_line) < 2 or not command_line[1].isdecimal():
-			print("Please enter PID of process to foreground")
-			return
-		pid = int(command_line[1])
-		if pid not in list(pid_to_process.keys()):
-			print("Please enter the PID of an existing process. Find the PIDs by using the \'jobs\' command.")
-			return
-		arguments = pid_to_process[pid].args
-		arguments.append('&')
-		pid_to_process[pid].send_signal(signal.SIGCONT)
-		p = sbp.Popen(arguments[:])
-		pid_to_process[p.pid] = p
-		pid_to_command[p.pid] = original
-	else:
-		# if not background:
-		try:
-			if piping:
-				pid_list, full_pid, process_list = pipe(commands_to_pipe)
-				pid_to_process[full_pid] = process_list
-				pid_to_command[full_pid] = original
+def subcommand_chain(commands, first_in, last_out):
+	# print(commands)
+	process = sbp.Popen(commands[0], stdin=first_in, stdout=sbp.PIPE)
+	pid_list = [process.pid]
+	process_list = [process]
+	args = [process.communicate()[0].decode("utf-8")]
+	# print()
+	for i in commands[1:-1]:
+		previous = process
+		if type(i) is not list:
+			i = [i]
+		i.extend(args)
+		# print(i)
+		# echo $(seq 1 10)
+		process = sbp.Popen(i, stdout=sbp.PIPE)
+		pid_list.append(process.pid)
+		process_list.append(process)
+		args = process.stdout
+		previous.stdout.close()
+		# print(args)
+	
+	last = commands[-1]
+	if type(last) is not list:
+			last = [last]
+	last.extend(args)
+	process = sbp.Popen(last, stdout=last_out)
+	pid_list.append(process.pid)
+	process_list.append(process)
+	# args = process.communicate()[0].decode("utf-8")
+	return pid_list, tuple(pid_list), process_list
 
-				if not background:
-					# print("not background")
-					# result_available = threading.Event()
-					# thread = threading.Thread(target=pipe_wait, args=((process_list,)))
-					# thread.start()
-					# thread.join()
-					# pipe_wait(process_list)
-					# print("x,y,z")
-					print(process_list[-1].communicate()[0].decode("utf-8"))
-					# process_list[-1].stdout.flush()
-			else:
-				p = sbp.Popen(command_line, stdin=in_redirect, stdout=out_redirect, stderr=sbp.STDOUT)
-				pid_to_process[p.pid] = p
-				pid_to_command[p.pid] = original
-				# processes[p.pid] = [p, original]
-				if not background:
-					p.communicate()
-				# else:
-				# 	python -m SimpleHTTPServer 8080 &
-				# 	p = sbp.Popen(command_line, shell=True,
-				#stdin=None, stdout=None, stderr=None, close_fds=True)
-				# 	p.wait()
-				# print(p.pid)
-				# p.communicate()
-				# print(out)
-		except PermissionError:
-			print("Permission Error: not a binary command or builtin or one you do not have acces to.")
-			return
+def subcommand_and_pipe(commands, first_in, last_out):
+	pid_list = []
+	process_list = []
+	first, last = commands[0], commands[-1]
+	# print(commands) 
+	# print(commands[1:-1])
+	if any(["$" in i for i in first]):
+		to_run = parse_subcommands(first)
+		print(to_run)
+		temp_pid_list, temp_full_pid, temp_process_list = subcommand_chain(to_run, first_in, sbp.PIPE)
+		pid_list.extend(temp_pid_list)
+		process_list.extend(temp_process_list)
+		process = process_list[-1]
+	else:
+		process = sbp.Popen(first, stdin = first_in, stdout=sbp.PIPE)
+		pid_list.append(process.pid)
+		process_list.append(process)
+
+	out = process.stdout
+	# print(process.communicate()[0])
+	for proc in commands[1:-1]:
+		previous = process
+		print(proc)
+		if any(["$" in i for i in proc]):
+			to_run = parse_subcommands(proc)
+			temp_pid_list, temp_full_pid, temp_process_list = subcommand_chain(to_run, out, sbp.PIPE)
+			pid_list.extend(temp_pid_list)
+			process_list.extend(temp_process_list)
+			process = process_list[-1]
+		else:
+			process = sbp.Popen(proc, stdin=out, stdout=sbp.PIPE)
+			pid_list.append(process.pid)
+			process_list.append(process)
+
+		out = process.stdout
+		previous.stdout.close()
+		print(out)
+
+	if any(["$" in i for i in last]):
+		to_run = parse_subcommands(last)
+		temp_pid_list, temp_full_pid, temp_process_list = subcommand_chain(to_run, out, last_out)
+		pid_list.extend(temp_pid_list)
+		process_list.extend(temp_process_list)
+		process = process_list[-1]
+	else:
+		process = sbp.Popen(first, stdin = out, stdout=last_out)
+		pid_list.append(process.pid)
+		process_list.append(process)
+
+	return pid_list, tuple(pid_list), process_list
 
 def loop():
 	cwd = os.getcwd()
@@ -236,22 +223,38 @@ def loop():
 		clean_processes(pid_to_process, pid_to_command)
 		# print(pid_to_process)
 		# try:
-		piping = False
-		cheat_shell = False
-		subcommands = False
-		in_redirect = None
-		out_redirect = None
-
+		piping, subcommands = False, False
+		in_redirect, out_redirect, first_in, last_out = None, None, None, None
 		command_line, background, original = get_input()
-		print(command_line)
+		# commands_to_run = command_line[:]
+		print("Initial parse: " + str(command_line))
 		
-		if "|" in command_line:
-			# find /home/dros -name "*.txt" | sort -n | uniq | cat
-			piping = True
+		piping = "|" in command_line
+		subcommands = any(["$" in i for i in command_line])
+		if piping and subcommands:
+			# echo $(echo $(echo $(echo $(seq 1 10)))) | cat
 			new_split = [l.split(' ') for l in ' '.join(command_line).split("|")]
-			commands_to_pipe = [new_split[0][0]] + [l[1] for l in new_split[1:-1]] + [new_split[-1][-1]]
+			print(new_split)
+			command_line = [new_split[0][:-1]] + [l[1:-1] for l in new_split[1:-1]] + [new_split[-1][1:]]
+
+			# command_line = parse_subcommands(command_line)
+			print(command_line)
+			last_out = sbp.PIPE
+		elif piping:
+			# find /home/dros -name "*.txt" | sort -n | uniq | cat
+			new_split = [l.split(' ') for l in ' '.join(command_line).split("|")]
+			print(new_split)
+			commands_to_pipe = [new_split[0][:-1]] + [l[1:-1] for l in new_split[1:-1]] + [new_split[-1][1:]]
+			# commands_to_run = commands_to_pipe
 			# print(new_split)
 			# print(commands_to_pipe)
+			last_out = sbp.PIPE
+		elif subcommands:
+			command_line = parse_subcommands(command_line)
+			last_out = sbp.PIPE
+		
+		# print(commands_to_pipe)
+		# print(command_line)
 		if "<" in command_line:
 			# new_split = [command_line[:command_line.index("<")]]
 			index = command_line.index("<")
@@ -260,7 +263,7 @@ def loop():
 			# file_name = new_split[1][1]
 			# print(command_line, file_name)
 			try:
-				in_redirect = open(file_name, 'r')
+				first_in = open(file_name, 'r')
 			except FileNotFoundError:
 				print(file_name + ": no such file or directory")
 				continue
@@ -269,16 +272,11 @@ def loop():
 			file_name = command_line[index+1:][0]
 			command_line = command_line[:index]
 			# print(command_line, file_name)
-			out_redirect = open(file_name, "w")
-
-		if any(["$" in i for i in command_line]):
-			commands_to_pipe = parse_subcommands(command_line)
-			piping = True
-
+			last_out = open(file_name, "w")
 
 		if len(command_line) < 1:
 			continue
-		
+
 		command = command_line[0]
 		if command == "exit":
 			for i in pid_to_process:
@@ -338,63 +336,40 @@ def loop():
 			pid_to_process[p.pid] = p
 			pid_to_command[p.pid] = original
 		else:
-			# if not background:
 			try:
-				if piping:
-					pid_list, full_pid, process_list = pipe(commands_to_pipe)
+				if subcommands and piping:
+					print("here\n")
+					pid_list, full_pid, process_list = subcommand_and_pipe(command_line, first_in, last_out)
+					pid_to_process[full_pid] = process_list
+					pid_to_command[full_pid] = original
+					if not background:
+						# print("not background")
+						print(process_list[-1].communicate()[0].decode("utf-8"))
+				elif piping:
+					pid_list, full_pid, process_list = pipe(commands_to_pipe, first_in, last_out)
 					pid_to_process[full_pid] = process_list
 					pid_to_command[full_pid] = original
 
 					if not background:
 						# print("not background")
-						# result_available = threading.Event()
-						# thread = threading.Thread(target=pipe_wait, args=((process_list,)))
-						# thread.start()
-						# thread.join()
-						# pipe_wait(process_list)
-						# print("x,y,z")
 						print(process_list[-1].communicate()[0].decode("utf-8"))
 						# process_list[-1].stdout.flush()
-				# elif subcommands:
-				# 	# print(parsed_command_list)
-				# 	pid_list = []
-				# 	process_list = []
-				# 	first = parsed_command_list.pop(-1)
-				# 	p = sbp.Popen(first, stdin=in_redirect, stdout=out_redirect, stderr=sbp.STDOUT)
-				# 	pid_list.append(p.pid)
-				# 	process_list.append(first)
-				# 	args = [p.communicate()]
-				# 	print(args)
-				# 	while parsed_command_list:
-				# 		current = [parsed_command_list.pop(-1)]
-				# 		# print(current+args)
-				# 		p = sbp.Popen(current+args, stdin=in_redirect, stdout=out_redirect, stderr=sbp.STDOUT)
-				# 		pid_list.append(p.pid)
-				# 		process_list.append(current)
-				# 		args = [p.communicate()]
-				# 	pid_to_process[tuple(pid_list)] = process_list
-				# 	pid_to_command[tuple(pid_list)] = original
-
-
+				elif subcommands:
+					pid_list, full_pid, process_list = subcommand_chain(command_line, first_in, last_out)
+					pid_to_process[full_pid] = process_list
+					pid_to_command[full_pid] = original
+					if not background:
+						# print("not background")
+						print(process_list[-1].communicate()[0].decode("utf-8"))
+						# process_list[-1].stdout.flush()
 				else:
-					p = sbp.Popen(command_line, stdin=in_redirect, stdout=out_redirect, stderr=sbp.STDOUT)
+					p = sbp.Popen(command_line, stdin=first_in, stdout=last_out, stderr=sbp.STDOUT)
 					pid_to_process[p.pid] = p
 					pid_to_command[p.pid] = original
 					# processes[p.pid] = [p, original]
 					if not background:
 						p.communicate()
-					# else:
-					# 	python -m SimpleHTTPServer 8080 &
-					# 	p = sbp.Popen(command_line, shell=True,
-					#stdin=None, stdout=None, stderr=None, close_fds=True)
-					# 	p.wait()
-					# print(p.pid)
-					# p.communicate()
-					# print(out)
 			except PermissionError:
 				print("Permission Error: not a binary command or builtin or one you do not have acces to.")
 				continue
-		# except KeyboardInterrupt:
-		# 	continue
-
 loop()
